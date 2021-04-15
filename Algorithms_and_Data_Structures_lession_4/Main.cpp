a327faf70622edab1b0bddb04b70b8c87ac67b7c
Kazantsev8 <vanskazancev@gmail.com>
2021-04-15 12:13:15 +0500

    4th task completed on C++

---
+++ Algorithms_and_Data_Structures_lession_4/Main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, current1, maxF = 0, currentCount = 0, maxCount = 1, prev;
+    cout << "How many elements your array will have?: ";
+    cin >> n;
+    cout << "Enter " << n << " numbers: " << endl;
+    cin >> maxF;
+    prev = maxF;
+    for (int current, i = 1; i < n; i++) {
+        cin >> current;
+        if (current - prev == 1) {
+            if (!currentCount)
+                current1 = prev, currentCount = 1;
+            currentCount++;
+            if (currentCount > maxCount)
+                maxF = current1, maxCount = currentCount;
+        }
+        else {
+            current1 = 0;
+            currentCount = 0;
+        }
+        prev = current;
+    }
+    cout << "Max count of increasing sequence is " << maxCount << endl;
+    cout << "Thats sequence is: ";
+    for (int i = maxF; i < maxF + maxCount; i++)
+        cout << i << " ";
+
+    return 0;
+}
